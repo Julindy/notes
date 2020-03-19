ef51c574e4fa12de5642ade8a10b86adf7065acd
-g <wangdongsheng@chinaedu.com>
2020-03-19 17:44:05 +0800

    1

--- jq/jq.js
+++ jq/jq.js
@@ -44,6 +44,17 @@ $(selector).toggleClass(className);
 $(selector).css(propertyName, value);
 $(selector).css({
   propertyName: value
-  
 });
 
+$(selector).children(filter) // 沿着 DOM 树向下遍历单一层级
+$(selector).contents() // 沿着 DOM 树向下遍历单一层级（包括被选元素的文本和注释节点）
+$(selector).find(filter) // 遍历多个层级
+$(selector).parent(filter) // 沿着 DOM 树向上遍历单一层级（不包括当前元素）
+$(selector).closest(filter) // 沿着 DOM 树向上遍历单一层级（包括当前元素）
+$(selector).parents(filter) // 遍历多个层级
+$(selector).prev() // 被选元素的前一个同级元素
+$(selector).next() // 被选元素的后一个同级元素
+$(selector).filter(filter) // 返回符合一定条件的元素
+$(selector).not(filter) // 返回不符合一定条件的元素
+$(selector).has(filter) // 返回有filter的selector
+$(selector).is(filter) // 返回是filter的selector
